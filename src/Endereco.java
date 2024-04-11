fddfc30de8d31a143d422040a6408186d7cf0c01
RafaelHR12 <147559750+RafaelHR12@users.noreply.github.com>
2024-04-11 16:50:37 -0300

    Second Commit

--- src/Endereco.java
+++ src/Endereco.java
@@ -6,45 +6,53 @@ public class Endereco {
 	private String bairro;
 	private String numero;
 	private String complemento;
-	
-	
+
 	public String getRua() {
 		return rua;
 	}
+
 	public void setRua(String rua) {
 		this.rua = rua;
 	}
+
 	public String getCep() {
 		return cep;
 	}
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public String getComplemento() {
 		return complemento;
 	}
+
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
-	
+
 	public boolean verificarEndereco(String rua, String cep, String bairro, String numero) {
-		if (rua.contains(" ") && cep.length()==8 && bairro.length()>2 && numero.length()>0) {
+		if (rua.contains(" ") && cep.length() == 8 && bairro.length() > 2 && numero.length() > 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 }
--- src/Operador.java
+++ src/Operador.java
@@ -4,30 +4,33 @@ public class Operador {
 	private String nome;
 	private String idOperador;
 	private String cpf;
-	
+
 	public String getNome() {
 		return this.nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getIdOperador() {
 		return this.idOperador;
 	}
+
 	public void setIdOperador(String idOperador) {
 		this.idOperador = idOperador;
 	}
-	
+
 	public String getHabilidades() {
 		return this.cpf;
 	}
+
 	public void setHabilidades(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public boolean verificarCadastro(String nome, String idOperador, String cpf) {
-		if (nome.contains(" ") && idOperador.length()>0 && cpf.length()==11) {
+		if (nome.contains(" ") && idOperador.length() > 0 && cpf.length() == 11) {
 			return true;
 		} else {
 			return false;
